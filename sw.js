10eda5cfd52475e75982be771d9c9f6e55dd5faf
Jamshaid Aslam <jamshaidaslam884@gmail.com>
2026-05-13 08:36:52 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'medpro-v9';
+const ASSETS = [
+  '/medpro-app/',
+  '/medpro-app/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
